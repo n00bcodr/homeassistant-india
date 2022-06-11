3320b079b6ccab9bf576ba07498b07c27596b4b3
root <root@homeassistant.local.hass.io>
2022-06-12 01:12:27 +0530

    files on date 2022-06-12, 01:12

--- tools/git_version_update.sh
+++ tools/git_version_update.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 cd /config
-
 git add .HA_VERSION
 git commit -m "$@"
+sleep 5s
 git push -u origin master
 
 exit
--- tools/ha_gitpush.sh
+++ tools/ha_gitpush.sh
@@ -1,14 +1,8 @@
-# Go to /config folder or 
-# Change this to your Home Assistant config folder if it is different
+#!/bin/bash
 cd /config
-
-# Add all files to the repository with respect to .gitignore rules
 git add .
-
-# Commit changes with message with current date stamp
 git commit -m "$@"
-
-# Push changes towards GitHub
+sleep 5s
 git push -u origin master
 
 exit
