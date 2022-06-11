8ac2b5ba706f3999e52e24a2dbca95bc2c3b2eb1
root <root@core-ssh.local.hass.io>
2022-06-11 14:46:10 +0530

    Core update to 2022.6.5

--- custom_components/hacs/hacs_frontend/__init__.py
+++ custom_components/hacs/hacs_frontend/__init__.py
@@ -1,12 +1,5 @@
-<<<<<<< Updated upstream
 """HACS Frontend"""
 from .version import VERSION
 
 def locate_dir():
-=======
-"""HACS Frontend"""
-from .version import VERSION
-
-def locate_dir():
->>>>>>> Stashed changes
     return __path__[0]
