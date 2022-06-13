dde4940da21a2051a66b84d37f868df5ea815efd
root <root@homeassistant.local.hass.io>
2022-06-13 19:47:42 +0530

    files on date 2022-06-13, 19:47

--- tools/git_core_update_push.sh
+++ tools/git_core_update_push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /config
 git config core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /config/.ssh/id_rsa -F /dev/null'
-git add .
+git add .HA_VERSION
 git commit -m "$@"
-git status
 git push -u origin master
+git tag -a "$@"
 exit
