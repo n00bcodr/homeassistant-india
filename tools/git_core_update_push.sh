7170f84315ed17141b793e0c67ea1dc41f1c50f3
root <root@a0d7b954-vscode.local.hass.io>
2022-06-20 23:35:21 +0530

    change to core update script

--- tools/git_core_update_push.sh
+++ tools/git_core_update_push.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd /config
 git config core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /config/.ssh/id_rsa -F /dev/null'
+ver=$(<.HA_VERSION)
 git add .HA_VERSION -f
-git commit -m "Core Version Update to $@"
+git commit -m "Core Version Update to $ver"
 git push -u origin master
-git tag -a "$@" -m "Core Version Update to $@"
-git push origin "$@"
+git tag -a "$ver" -m "Core Version Update to $ver"
+git push origin "$ver"
 exit
